831e4d28c1a1be36e82b0d1b9748eb0acfd6b0a3
Jack <jackmcgrath2k@gmail.com>
2025-04-22 21:10:54 +0100

    fix or something

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    // No "output: 'export'"
+    // No "experimental.appDir = false"
+  };
+  
+  module.exports = nextConfig;
+  
